d71d88a6e5ac11afe2944f3547099f860658563f
engin <o.enginoz@gmail.com>
2018-09-19 02:22:02 +0300

    düzenleme yapıldı

---
+++ dist/js/electron.js
@@ -0,0 +1,84 @@
+var electron = (function() {
+    'use strict';
+
+    var fs = require('fs');
+    var path = require('path');
+    window.$ = window.jQuery = require('jquery');
+
+    var electron = {
+        version: "0.0.1"
+    };
+
+    electron.imageFiles = [];
+    electron.imageCount = 0;
+    electron.effects = ["blind", "bounce", "clip", "drop", "explode", "fade", "fold", "highlight", "puff", "pulsate", "shake", "slide"];
+    electron.orderCount = 99999999;
+
+    electron.readDir = (dir, callback) => {
+        fs.readdir(dir, (err, files) => {
+            files.forEach((file) => {
+                electron.imageFiles.push(dir+file);
+                electron.imageCount++;
+            });
+
+        })
+        callback();
+    }
+
+    electron.readFile = (fileName, callback) => {
+        fs.readFile(fileName, (err, data) => {
+            if (err) {
+                alert("An error ocurred reading the file :" + err.message);
+                return;
+            }
+
+            callback(data,fileName);
+        });
+    };
+
+    electron.getImageFromFile = (filename, callback) => {
+
+        electron.readFile(filename, (data,fileName) => {
+            //get image file extension name
+            let extensionName = path.extname(fileName);
+
+            //convert image file to base64-encoded string
+            let base64Image = new Buffer(data, 'binary').toString('base64');
+
+            //combine all strings
+            let imgSrcString = `data:image/${extensionName.split('.').pop()};base64,${base64Image}`;
+
+            callback(imgSrcString);
+        });
+
+    }
+
+    electron.fillImageToId = (fileName) => {
+        electron.getImageFromFile(fileName, (data) => {
+            document.getElementById("img").src = data;
+        })
+
+    }
+
+    electron.getRandomInt = function(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    electron.callback = function() {
+        $( "#menuDiv" ).removeAttr( "style" ).fadeIn();
+
+    }
+
+    electron.getmenuImgByOrder = function () {
+        if(++electron.orderCount > (electron.imageCount-1))
+            electron.orderCount = 0
+        return electron.orderCount;
+    }
+
+    electron.explode = function() {
+        $( "#menuDiv" ).effect( electron.effects[electron.getRandomInt(12)] , null, 500, electron.callback );
+        electron.fillImageToId(electron.imageFiles[electron.getmenuImgByOrder()]);
+    }
+
+    return electron;
+})();
---
+++ dist/js/menuList.js
@@ -0,0 +1,33 @@
+var tickCount = 1;
+
+var readDir
+
+var timerId = setTimeout(function tick() {
+    console.log(tickCount);
+    if (tickCount % 12 === 0) {
+        users.getAllUsers(function () {
+            users.fillList(function () {
+            })
+        });
+    }
+    else if (tickCount % 10800 === 0) {
+        users.checkIn(function () {
+            users.getAllUsers(function () {
+                users.fillList(function () {
+                })
+            })
+        });
+    }
+    else
+        electron.explode();
+    tickCount++;
+    timerId = setTimeout(tick, 1000); // (*)
+}, 1000);
+
+users.checkIn(function () {
+    users.getAllUsers(function () {
+        users.fillList(function () {
+        })
+    })
+});
+electron.readDir("/root/images/", electron.explode);
