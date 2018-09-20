9b988231f5a2298653b88a62b81b7f088a4c65ff
engin <o.enginoz@gmail.com>
2018-09-20 23:37:14 +0300

    düzenleme

--- dist/js/menuList.js
+++ dist/js/menuList.js
@@ -22,7 +22,7 @@ var timerId = setTimeout(function tick() {
     timerId = setTimeout(tick, 5000); // (*)
 }, 5000);
 
-var jqxhr = $.getJSON( "https://social.oenginoz.com/buca.json", function(data) {
+var jqxhr = $.getJSON( "https://social.oenginoz.com/buca.json", {_: new Date().getTime()}, function(data) {
     if(data.venueId === undefined)
         return;
     users.venueId = data.venueId;
@@ -36,5 +36,11 @@ var jqxhr = $.getJSON( "https://social.oenginoz.com/buca.json", function(data) {
     $("#brandh").html("<img id=\"brandImg\" src=\""+data.brandImg+"\" class=\"img-circle elevation-2\" style=\"height: 75px; width: 75px;\"/> "+ data.brandName);
     electron.readDir("/root/images/", electron.explode);
 }).fail(function(err) {
+    $("#swarmFooter").remove();
+    $("#brandh").remove();
+    $("#cards").remove();
+    $("#menus").removeClass("col-lg-9");
+    $("#menus").addClass("col-12");
+    electron.readDir("/root/images/", electron.explode);
     console.log(err );
 });
